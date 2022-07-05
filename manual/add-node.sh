d7ec71ebe68143b52315d4ad0324a3ffb9051471
Jonny <ji394su311203106@gmail.com>
2022-07-06 00:00:30 +0800

    update add and delete node shell

---
+++ manual/add-node.sh
@@ -0,0 +1,23 @@
+
+# get requirepass from redis-cluster
+requirepass=$(cat redis-cluster.conf | grep requirepass | cut -d' ' -f2)
+# get masterauth from redis-cluster
+masterauth=$(cat redis-cluster.conf | grep masterauth | cut -d' ' -f2)
+
+# get first master node id
+firstMasterNode=$(docker exec -t manual_redis-node1_1 redis-cli -h manual_redis-node1_1 -p 6379 -a $requirepass cluster nodes| grep -v fail | grep master | head -1 | cut -d' ' -f1)
+
+# create master redis container
+docker run -d --name redis-new-master01 --network manual_default redis:7.0 --cluster-enabled yes --appendonly yes  --requirepass $requirepass --masterauth $masterauth
+# create slave redis container
+docker run -d --name redis-new-slave01 --network manual_default redis:7.0 --cluster-enabled yes --appendonly yes  --requirepass $requirepass --masterauth $masterauth
+
+# add master to cluster
+docker exec -it manual_redis-node1_1 redis-cli --cluster add-node redis-new-master01:6379  manual_redis-node6_1:6379 -a $requirepass
+
+# get new master node id
+newMasterNode=$(docker exec -t manual_redis-node1_1 redis-cli -h redis-new-master01 -p 6379 -a $requirepass cluster nodes | grep myself | cut -d' ' -f1)
+
+# reshard slots to new master
+docker exec -it manual_redis-node1_1 redis-cli --cluster reshard redis-new-master01:6379 --cluster-from $firstMasterNode --cluster-to $newMasterNode --cluster-slots 1 --cluster-yes  -a $requirepass
+
---
+++ manual/delete-node.sh
@@ -0,0 +1,23 @@
+
+# get requirepass from redis-cluster
+requirepass=$(cat redis-cluster.conf | grep requirepass | cut -d' ' -f2)
+
+# get master node id
+masterNode=$(docker exec -t manual_redis-node1_1 redis-cli -h redis-new-master01 -p 6379 -a $requirepass cluster nodes | grep -v myself | grep master | head -1 | cut -d' ' -f1)
+
+# get delete master node id
+deleteMasterNode=$(docker exec -t manual_redis-node1_1 redis-cli -h redis-new-master01 -p 6379 -a $requirepass cluster nodes | grep myself | head -1 | cut -d' ' -f1)
+
+# get delete master node slot
+deleteMasterNodeSlot=$(docker exec -t manual_redis-node1_1 redis-cli --cluster info redis-new-master01:6379 -a $requirepass | grep redis-new-master01 | cut -d'|' -f2 | sed 's/slots//g')
+
+# reshard deleteNode to other node
+docker exec -it manual_redis-node1_1 redis-cli --cluster reshard redis-new-master01:6379 --cluster-from $deleteMasterNode --cluster-to $masterNode --cluster-slots $deleteMasterNodeSlot --cluster-yes  -a $requirepass
+
+
+# delete master node
+docker exec -t manual_redis-node1_1 redis-cli --cluster del-node redis-new-master01:6379 $deleteMasterNode -a $requirepass
+
+# remove container
+docker rm -f redis-new-slave01
+docker rm -f redis-new-master01
